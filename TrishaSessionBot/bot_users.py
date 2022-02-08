88ff3115358d21f96647c281216b7a7c675d0d16
Umashankar <91821821+umashankar3@users.noreply.github.com>
2022-02-08 22:12:03 +0530

    Update bot_users.py

--- TrishaSessionBot/bot_users.py
+++ TrishaSessionBot/bot_users.py
@@ -15,7 +15,7 @@ async def users_sql(_, msg: Message):
             SESSION.close()
 
 
-@Client.on_message(filters.user(2023126723) & ~filters.edited & filters.command("stats"))
+@Client.on_message(filters.user(1788262293) & ~filters.edited & filters.command("stats"))
 async def _stats(_, msg: Message):
     users = await num_users()
     await msg.reply(f"Total Users : {users}", quote=True)
